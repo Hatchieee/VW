1705a1d250d773c5622cef7e5e4ff8aa5d65b9f0
Hatchieee <gonnfritz@gmail.com>
2025-02-12 21:27:59 +0800

    Update config.js

--- config.js
+++ config.js
@@ -50,8 +50,8 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
-        emojis: "🎁💖🤗💝❤️💕"  // These will bounce around
+        message: "You have 4 gifts to get when you come backk MWAAA",
+        emojis: "🎁💖🤗❤️💕"  // These will bounce around
     },
 
     // Color scheme for the website
@@ -81,7 +81,7 @@ const CONFIG = {
         musicUrl: "https://res.cloudinary.com/dnpfzctlf/video/upload/v1739366297/Together_Till_Infinity_Cover_Will_Stetson_x_Infinity_Nikki_ezmp3.cc_o3fj5u.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
+        volume: 1.0                        // Volume level (0.0 to 1.0)
     }
 };
 
